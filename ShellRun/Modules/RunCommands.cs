7b43861e6e8404c7b3e4329e43ee01f99d0a0dca
DivineBytes <Admin@DivineBytes>
2023-02-18 17:10:50 -0600

    Update

--- ShellRun/Modules/RunCommands.cs
+++ ShellRun/Modules/RunCommands.cs
@@ -1,12 +1,13 @@
 ﻿namespace ShellRun.Modules
 {
-    /// <summary>
-    /// The run commands.
-    /// </summary>
-    /// <remarks>
-    /// Links: <br /><see href="https://ss64.com/nt/run.html"></see><br /><see href="https://techcult.com/complete-list-of-windows-11-run-commands/"></see>
-    /// </remarks>
-    public class RunCommands
-    {
-    }
+    ///// <summary>
+    ///// The run commands.
+    ///// </summary>
+    ///// <remarks>
+    ///// Links: <br /><see href="https://ss64.com/nt/run.html"></see><br /><see href="https://techcult.com/complete-list-of-windows-11-run-commands/"></see>
+    ///// </remarks>
+    //public class RunCommands
+    //{
+
+    //}
 }
